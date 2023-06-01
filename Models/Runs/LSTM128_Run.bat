cf71ab87d81d7ba8b02244b129564c5d18ee9445
njfredri <njfredri@uark.edu>
2023-06-01 15:29:58 -0500

    finished making separate cell and hidden states for the c model. Everything seems to match up so far with the python model.

--- Models/Runs/LSTM128_Run.bat
+++ Models/Runs/LSTM128_Run.bat
@@ -8,3 +8,4 @@ cd ../Pytorch_Models
 python lstm128.py ../WeightsAndBiases/lstm128.json
 cd ../
 python compare_outputs.py  ./outputs/lstm128_c_output.txt ./outputs/lstm128_python_output.txt ./outputs/lstm128_run.output.txt
+cd Runs
